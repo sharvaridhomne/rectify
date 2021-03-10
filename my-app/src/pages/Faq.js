5149e09d958b4c9fa919a2023494f496b89baf8d
Sharvari Dhomne <sharvari.dhomne22@gmail.com>
2021-03-10 13:53:45 +0530

    faq

--- my-app/src/pages/Faq.js
+++ my-app/src/pages/Faq.js
@@ -1,29 +1,21 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
+
 import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Link from "@material-ui/core/Link";
+
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { withStyles } from "@material-ui/core/styles";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import StarIcon from "@material-ui/icons/Star";
+
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+
 
 const useStyles = makeStyles((theme) => ({
   hero: {
@@ -43,101 +35,274 @@ const useStyles = makeStyles((theme) => ({
       fontSize: "4rem",
     },
   },
-  root: {
-    width: "100%",
-    maxWidth: "100%",
-    backgroundColor: theme.palette.background.paper,
+  nested: {
+    paddingLeft: theme.spacing(4),
   },
-  media: {
-    height: 200,
+  root: {
+    root: {
+      width: "100%",
+    },
+  },
+
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
   },
 }));
 
 export default function Faq() {
   const classes = useStyles();
+  const preventDefault = (event) => event.preventDefault();
 
   return (
     <div>
       <Box className={classes.hero}>
-        {/* <Box>
+        {/* <Typography>
             Repair your Credit Score and ensure your Loan Eligibility with Rectify
             Credit In optimal time
-          </Box> */}
+          </Typography> */}
         {/* <Box>reliable experts in rebuilding your Credit Score </Box> */}
       </Box>
-      <Container
-        maxWidth="md"
-        style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
-      >
-        <Card className={classes.root}>
-          <CardActionArea>
-            {/* <CardMedia
-                className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
-              /> */}
-            <CardContent>
-              <Typography gutterBottom variant="h4" component="h1">
-                Just click on the question to scroll down to the answer!
+      <div style={{ backgroundColor: "#cfe8fc" }}>
+        <Container>
+          <Typography gutterBottom variant="h4" component="h1">
+            Just click on the question to scroll down to the answer!
+          </Typography>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>
+                1. How does Rectify Credit help me?
               </Typography>
-              <List
-                component="nav"
-                className={classes.root}
-                aria-label="contacts"
-              >
-                <ListItem button>
-                  <ListItemIcon>
-                    <StarIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="How does Rectify Credit help me?" />
-                </ListItem>
-                <ListItem button>
-                  <ListItemIcon>
-                    <StarIcon />
-                  </ListItemIcon>
-                  <ListItemText inset primary="Where do I begin?" />
-                </ListItem>
-                <ListItem button>
-                  <ListItemIcon>
-                    <StarIcon />
-                  </ListItemIcon>
-                  <ListItemText inset primary="Where do I begin?" />
-                </ListItem>
-                <ListItem button>
-                  <ListItemIcon>
-                    <StarIcon />
-                  </ListItemIcon>
-                  <ListItemText inset primary="How much will it costs me?" />
-                </ListItem>
-                <ListItem button>
-                  <ListItemIcon>
-                    <StarIcon />
-                  </ListItemIcon>
+            </AccordionSummary>
+            <AccordionDetails>
+              <List component="nav" aria-label="secondary mailbox folders" >
+                <ListItem >
                   <ListItemText
-                    inset
-                    primary="What does Rectify Credit offer?"
+                    color="textSecondary"
+                    primary="With RectifyCredit you get your Credit on track with Simple Steps:"
+                  />
+                  
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    color="textSecondary"
+                    secondary="Just register yourself on our web Site RectifyCredit.com"
                   />
                 </ListItem>
-                <ListItem button>
-                  <ListItemIcon>
-                    <StarIcon />
-                  </ListItemIcon>
-                  <ListItemText inset primary="How long does it take?" />
-                </ListItem>
-                <ListItem button>
-                  <ListItemIcon>
-                    <StarIcon />
-                  </ListItemIcon>
+                <ListItem>
                   <ListItemText
-                    inset
-                    primary="Where do I send the Credit Report?"
+                    color="textSecondary"
+                    secondary="Send your personal Credit report to us on:   info@rectifycredit.com"
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    color="textSecondary"
+                    secondary="Call your RectifyCredit advisor on 8082554356/57/58/59"
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    color="textSecondary"
+                    secondary="RectifyCredit Advisors will read you CIBIL report and help you resolving CIBIL issues."
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    color="textSecondary"
+                    secondary="RectifyCredit Avisors will handhold you through the credit Report Restoration process."
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    color="textSecondary"
+                    secondary="Once your CIBIL Report is repaired you can confidently apply for any loan from any Bank."
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    color="textSecondary"
+                    secondary="Just register yourself on our web Site RectifyCredit.com"
                   />
                 </ListItem>
               </List>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Container>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>
+                2. Where do I begin?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary">
+                You can do it right here, just now by registering on our
+                website,
+                <Link
+                  href="https://www.rectifycredit.com"
+                  onClick={preventDefault}
+                >
+                  www.rectifycredit.com
+                </Link>{" "}
+                . On registering, you will receive an email with our contact
+                details and simple instructions to apply for the credit report
+                and sharing it with us. Once we receive your report our team
+                will do a thorough analysis and explain the rectification
+                process.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>
+                3. How much will it cost me?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary">
+                Our basic fees start from Rs 5000 ( plus taxes as applicable).
+                We charge in advance. These fees are normally based on the
+                effort and support that is needed to assist you to achieve the
+                changes.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>
+                4. What does Rectify Credit offer?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary">
+                We offer the most economical and sure shot approach to achieve
+                your reasonable credit scores mean. We do a detailed analysis of
+                your credit report to understand the issues that could be
+                harming your credit report. We then engage in personalized
+                advise, and handholding on emails and telephones till the
+                defined steps are completed.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>
+                5. How long does it take?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary">
+                The entire process of interaction is generally slow and can take
+                time in months typically 2-3 months. The time taken can be less
+                if the issues are simpler in nature then may be couple of weeks
+                should be enough. But note the entire process can start only
+                after your send us your credit report pulled by you!
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>
+                6. Where do I begin?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary">
+                You can do it right here, just now by registering on our
+                website, www.rectifycredit.com . On registering, you will
+                receive an email with our contact details and simple
+                instructions to apply for the credit report and sharing it with
+                us. Once we receive your report our team will do a thorough
+                analysis and explain the rectification process.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>
+                7. Where do I send the Credit Report?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary">
+                Once you receive the report, you could email us the report to:
+                <Link
+                  href="https://reports@rectifycredit.com"
+                  onClick={preventDefault}
+                >
+                  reports@rectifycredit.com
+                </Link>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>
+                8. Will pulling out my own credit report create problem for me in
+                any ways?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary">
+                When you pull out your own credit report it will not create
+                problem for you in any possible way. Your credit score will not
+                be effected.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>
+                9. I am afraid if my information will be shared with other
+                agencies?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography color="textSecondary">
+                We respect our customers privacy and do not share your
+                information with any one unless you direct us to do so in
+                writing.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Container>
+      </div>
     </div>
   );
 }
