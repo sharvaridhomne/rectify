7fc0e4aa6bd4724a4af237d2f82014e5e04940d8
Sharvari Dhomne <sharvari.dhomne22@gmail.com>
2021-03-10 11:12:07 +0530

    routes added

--- my-app/src/routes/index.js
+++ my-app/src/routes/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import SignUp from "../pages/SignUp";
+import Creditrepair from "../pages/Creditrepair";
+import CreditEducation from "../pages/CreditEducation";
+import Contactus from "../pages/ContactUs";
+import Faq from "../pages/Faq";
+
+
+
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/register" exact component={SignUp} />
+      <Route path="/creditrepairservices" exact component={Creditrepair} />
+      <Route path="/crediteducation" exact component={CreditEducation} />
+      <Route path="/contactus" exact component={Contactus} />
+      <Route path="/faq" exact component={Faq} />
+
+
+
     </Switch>
   );
 }
