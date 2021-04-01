4442f3f94f98ba6647c35b8ddc4f8ae8ce6aa208
Sharvari Dhomne <sharvari.dhomne22@gmail.com>
2021-04-01 16:44:44 +0530

    ..

--- my-app/src/pages/PlaceToVisit2.js
+++ my-app/src/pages/PlaceToVisit2.js
@@ -40,13 +40,13 @@ const useStyles = makeStyles((theme) => ({
     color: "#fff",
     fontSize: "4.5rem",
   },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+  // form: {
+  //   width: "100%", // Fix IE 11 issue.
+  //   marginTop: theme.spacing(3),
+  // },
+  // submit: {
+  //   margin: theme.spacing(3, 0, 2),
+  // },
 }));
 export default function PlaceToVisit2() {
   const classes = useStyles();
