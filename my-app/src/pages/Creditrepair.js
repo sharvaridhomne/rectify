44d873f2126f6d973427d7c1dd2b8d6c9ed33da6
Sharvari Dhomne <sharvari.dhomne22@gmail.com>
2021-04-01 16:23:50 +0530

    credit repair updated

--- my-app/src/pages/Creditrepair.js
+++ my-app/src/pages/Creditrepair.js
@@ -9,23 +9,23 @@ import PlaceToVisit1 from "../pages/PlaceToVisit1";
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: "100vh",
-    backgroundImage: `url(${process.env.PUBLIC_URL + "assets/home.jpg"})`,
+    backgroundImage: `url(${process.env.PUBLIC_URL + "assets/bg6.jpg"})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
   },
   // spacing: 4,
   colorText: {
-    color: "#5AFF3D",
+    color: "#33691e",
   },
   container: {
     textAlign: "center",
   },
   title: {
-    color: "#fff",
+    color: "#64dd17",
     fontSize: "3.5rem",
   },
   goDown: {
-    color: "#5AFF3D",
+    color: "#33691e",
     fontSize: "4rem",
   },
   cardimage: {
@@ -60,8 +60,10 @@ export default function Home() {
             <br />
 
             <h1 className={classes.title}>
-              Repair your Credit Score <br />and ensure your <br />
-              <span className={classes.colorText}>Loan Eligiblity with<br/>
+              Repair your Credit Score <br />
+              and ensure your <br />
+              <span className={classes.colorText}>
+            Loan Eligiblity with<br/>
                Rectify Credit in Optimal Time.</span>
             </h1>
             <Scroll to="place-to-visit1" smooth={true}>
